86c27f0572af44c16068b5351ab1dc4ac50d157c
GOECO <ajcgroup@gmail.com>
2026-01-16 13:25:15 +0700

    Wrap app with ProviderScope

--- uvfl_wallet/lib/main.dart
+++ uvfl_wallet/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(const UvflApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: UvflApp()));
 }
